1862a0328366d5ef2d590ffe5241deaa80654140
Yiming Lu <luyimingboy@163.com>
2024-08-02 20:04:19 +0800

    fixed error

--- project/toymodel_spinglass.jl
+++ project/toymodel_spinglass.jl
@@ -18,7 +18,7 @@ mutable struct spinglassmodel
     const edges::Vector{Tuple{Int,Int,Float64}}
     const onsite::Vector{Float64}
     M::Vector{Vector{Float64}}
-    function spinglassmodel(n::Int, m::Int, edges::Vector{Tuple{Int,Int,Float64}}, onsite::Vector{Float64}, M::Vector{Vector{Float64}} = fill([1.0, 0.0, 0.0], length(onsite)))
+    function spinglassmodel(n::Int, m::Int, edges::Vector{Tuple{Int,Int,Float64}}, onsite::Vector{Float64}, M::Vector{Vector{Float64}} = fill([-1.0, 0.0, 0.0], length(onsite)))
         new(n, m, edges, onsite, M)
     end
 end
@@ -58,6 +58,12 @@ function spinglass_mapping(n::Int, m::Int)
     return spinglassmodel(n, m, edges, onsites)
 end
 
+function freeze_input!(sp::spinglassmodel)
+    for i in 1:sp.n
+        sp.onsite[i] += 20.0
+    end
+end
+
 
 # H(i) = -hx̂ + ∑_j J_{i,j} M_j^z ẑ + onsite ẑ
 # ̇M = H \cross M
@@ -68,6 +74,16 @@ end
 function integrator(sp::spinglassmodel, H::Vector{Vector{Float64}})
     Mdot = crossdot.(H, sp.M)
     return Mdot
+end
+
+function instantaneous_field(sp::spinglassmodel, t, T, Vtrans::Vector{Float64})
+    H = [[-Vtrans[i] .* ((T-t)/T), 0.0, sp.onsite[i] .* (t/T)] for i in 1:length(sp.onsite)]
+    for edge in sp.edges
+        i, j, w = edge
+        H[i][3] += w * sp.M[j][3] * (t/T)
+        H[j][3] += w * sp.M[i][3] * (t/T)
+    end
+    return H
 end
 
 function integrator(sp::spinglassmodel, t, T, Vtrans::Vector{Float64}) # evaluate F(t, y)
@@ -86,39 +102,58 @@ function integrator(sp::spinglassmodel, t, T, Vtrans::Vector{Float64}) # evaluat
     Mydot -= - Vtrans .* [u[3] for u in sp.M] .* ((T-t)/T)
     Mzdot = - Vtrans .* [u[2] for u in sp.M] .* ((T-t)/T)
 
-    # H = [[-Vtrans[i] .* ((T-t)/T), 0.0, sp.onsite[i] .* (t/T)] for i in 1:length(sp.onsite)]
-    # for edge in sp.edges
-    #     i, j, w = edge
-    #     H[i][3] += w * sp.M[j][3] * (t/T)
-    #     H[j][3] += w * sp.M[i][3] * (t/T)
-    # end
-    # Mdot_bf = integrator(sp, H)
-    # for i in 1:length(sp.onsite)
-    #     # @info "i = $i, Mdot = $(Mdot_bf[i]), Mxdot = $(Mxdot[i]), Mydot = $(Mydot[i]), Mzdot = $(Mzdot[i])"
-    #     @assert abs(Mxdot[i] - Mdot_bf[i][1]) <= 1e-5
-    #     @assert abs(Mydot[i] - Mdot_bf[i][2]) <= 1e-5
-    #     @assert abs(Mzdot[i] - Mdot_bf[i][3]) <= 1e-5
-    # end
-
     return [Mxdot, Mydot, Mzdot]
 end
 
-function runge_kutta_integrate!(sp::spinglassmodel, dt, T, Vtrans::Vector{Float64})
-    t0 = 0
-    while t0 < T
-        delta_t = min(dt, T-t0)
-        origin_M = copy(sp.M)
-        k1 = integrator(sp, t0, T, Vtrans)
-        sp.M = [renorm([origin_M[i][1] + k1[1][i] * delta_t / 2, origin_M[i][2] + k1[2][i] * delta_t / 2, origin_M[i][3] + k1[3][i] * delta_t / 2]) for i in 1:length(sp.onsite)]
-        k2 = integrator(sp, t0 + delta_t / 2, T, Vtrans)
-        sp.M = [renorm([origin_M[i][1] + k2[1][i] * delta_t / 2, origin_M[i][2] + k2[2][i] * delta_t / 2, origin_M[i][3] + k2[3][i] * delta_t / 2]) for i in 1:length(sp.onsite)]
-        k3 = integrator(sp, t0 + delta_t / 2, T, Vtrans)
-        sp.M = [renorm([origin_M[i][1] + k3[1][i] * delta_t, origin_M[i][2] + k3[2][i] * delta_t, origin_M[i][3] + k3[3][i] * delta_t]) for i in 1:length(sp.onsite)]
-        k4 = integrator(sp, t0 + delta_t, T, Vtrans)
-        real_k = (k1 .+ 2*k2 .+ 2*k3 .+ k4) ./ 6 * delta_t
-        sp.M = [renorm([origin_M[i][1] .+ real_k[1][i], origin_M[i][2]+real_k[2][i], origin_M[i][3]+real_k[3][i]]) for i in 1:length(sp.onsite)]
+function runge_kutta_singlejump!(sp::spinglassmodel, t0, delta_t, T, Vtrans::Vector{Float64})
+    origin_M = copy(sp.M)
+    k1 = integrator(sp, t0, T, Vtrans)
+    sp.M = [renorm([origin_M[i][1] + k1[1][i] * delta_t / 2, origin_M[i][2] + k1[2][i] * delta_t / 2, origin_M[i][3] + k1[3][i] * delta_t / 2]) for i in 1:length(sp.onsite)]
+    k2 = integrator(sp, t0 + delta_t / 2, T, Vtrans)
+    sp.M = [renorm([origin_M[i][1] + k2[1][i] * delta_t / 2, origin_M[i][2] + k2[2][i] * delta_t / 2, origin_M[i][3] + k2[3][i] * delta_t / 2]) for i in 1:length(sp.onsite)]
+    k3 = integrator(sp, t0 + delta_t / 2, T, Vtrans)
+    sp.M = [renorm([origin_M[i][1] + k3[1][i] * delta_t, origin_M[i][2] + k3[2][i] * delta_t, origin_M[i][3] + k3[3][i] * delta_t]) for i in 1:length(sp.onsite)]
+    k4 = integrator(sp, t0 + delta_t, T, Vtrans)
+    real_k = (k1 .+ 2*k2 .+ 2*k3 .+ k4) ./ 6 * delta_t
+    sp.M = [renorm([origin_M[i][1] .+ real_k[1][i], origin_M[i][2]+real_k[2][i], origin_M[i][3]+real_k[3][i]]) for i in 1:length(sp.onsite)]
+end
+
+function runge_kutta_integrate!(sp::spinglassmodel, dt::Float64, T::Float64, Vtrans::Vector{Float64}; T_end = nothing)
+    t0 = 0.0
+    if T_end == nothing
+        T_end = T
+    end
+    sp_error_moniter = spinglass_mapping(sp.n, sp.m)
+    total_error = 0.0
+    while t0 < T_end
+        delta_t = min(dt, T_end-t0)
+        # now calculate jump with 2-step
+        # sp_error_moniter.M = copy(sp.M)
+        # runge_kutta_singlejump!(sp_error_moniter, t0, delta_t/2, T, Vtrans)
+        # runge_kutta_singlejump!(sp_error_moniter, t0, delta_t/2, T, Vtrans)
+        # now calculate jump with 1-step
+
+        # inst_H = instantaneous_field(sp, t0, T, Vtrans)
+        # inst_norm = [sqrt(sum(inst_H[i] .* inst_H[i])) for i in 1:length(sp.onsite)]
+        # inst_dot_norm = [sum(inst_H[i] .* sp.M[i]) for i in 1:length(sp.onsite)]
+        # inst_cos_theta = inst_dot_norm ./ inst_norm
+        # inst_min_cos_theta = minimum(inst_cos_theta)
+        # @info "t = $t0, inst_min_cos_theta = $inst_min_cos_theta"
+        # if inst_min_cos_theta < 0
+        #     @info "inst_H = $inst_H"
+        #     @info "sp.M = $(sp.M)"
+        #     error("err")
+        # end
+
+        runge_kutta_singlejump!(sp, t0, delta_t, T, Vtrans)
+        # now calculate local truncation error
+        # local_error = maximum(sp.M .- sp_error_moniter.M) / (1 - 2^-4)
+        # total_error += maximum(local_error)
+        # @assert maximum(local_error) <= 1e-10
+        # @info "t = $t0, local_error = $local_error"
         t0 += delta_t
     end
+    # @info "total_error = $total_error"
 end
 
 function euclidean_integrate!(sp::spinglassmodel, dt, T, Vtrans::Vector{Float64})
@@ -164,49 +199,7 @@ function sp_energy(sp::spinglassmodel, t, T, Vtrans)
     return ret
 end
 
-@testset "runge_kutta_with_euclidean" begin
-    sp_r = spinglass_mapping(3, 2)
-    sp_e = spinglass_mapping(3, 2)
-    Vtrans = fill(1.0, length(sp_r.onsite))
-    runge_kutta_integrate!(sp_r, 1e-2, 10, Vtrans)
-    euclidean_integrate!(sp_e, 1e-6, 10, Vtrans)
-
-    for i in 1:length(sp_r.onsite)
-        for j in 1:3
-            @test abs(sp_r.M[i][j] - sp_e.M[i][j]) <= 1e-2
-        end
-    end
-end
-
-@testset "runge_kutta_error" begin
-    sp_r1 = spinglass_mapping(3, 3)
-    sp_r2 = spinglass_mapping(3, 3)
-    Vtrans = fill(1.0, length(sp_r1.onsite))
-    runge_kutta_integrate!(sp_r1, 1e-5, 60, Vtrans)
-    runge_kutta_integrate!(sp_r2, 1e-4, 60, Vtrans)
-    for i in 1:length(sp_r1.onsite)
-        @info "sp_r1.M = $(sp_r1.M[i]), sp_r2.M = $(sp_r2.M[i])"
-        @info "delta = $(sp_r1.M[i] .- sp_r2.M[i])"
-    end
-
-    for i in 1:length(sp_r1.onsite)
-        for j in 1:3
-            @test abs(sp_r1.M[i][j] - sp_r2.M[i][j]) <= 1e-2
-        end
-    end
-end
-
-function check_vaild_time(sp::spinglassmodel, T, Vtrans)
-    @info "begin running runge_kutta"
-    runge_kutta_integrate!(sp, min(1e-2, T/1e5), T, Vtrans)
-    # @info "sp.M = $(sp.M)"
-    for i in 1:length(sp.onsite)
-        if 1 - abs(sp.M[i][3]) > 1e-1
-            return false
-        end
-        # @test 1 - abs(sp.M[i][3]) <= 1e-1
-    end
-    @info "successfully turn into final hamiltonian"
+function check_vaild(sp::spinglassmodel)
     state = [sp.M[i][3] > 0 ? 1 : 0 for i in 1:length(sp.onsite)]
     for j in 1:sp.m-1
         for i in 1:sp.n
@@ -222,13 +215,28 @@ function check_vaild_time(sp::spinglassmodel, T, Vtrans)
     return true
 end
 
+
+function check_vaild_time(sp::spinglassmodel, T, Vtrans)
+    @info "begin running runge_kutta"
+    runge_kutta_integrate!(sp, min(1e-2, T/1e5), T, Vtrans)
+    # @info "sp.M = $(sp.M)"
+    for i in 1:length(sp.onsite)
+        if 1 - abs(sp.M[i][3]) > 1e-1
+            return false
+        end
+    end
+    @info "successfully turn into final hamiltonian"
+    return check_vaild(sp)
+end
+
 function evaluate_adiabatic_time(n, m)
     Vtrans = fill(1.0, n*m + n*(m-1))
-    adiabatic_time = 0
-    max_try = 1
+    adiabatic_time = 0.0
+    max_try = 1.0
     while max_try > 0
         next_try = max_try
         sp = spinglass_mapping(n, m)
+        freeze_input!(sp)
         @info "Stage1, max_try = $max_try, next_try = $next_try, checking if vaild"
         flag = check_vaild_time(sp, next_try, Vtrans)
         if flag == true
@@ -245,6 +253,7 @@ function evaluate_adiabatic_time(n, m)
         max_try /= 2
         next_try = adiabatic_time + max_try
         sp = spinglass_mapping(n, m)
+        freeze_input!(sp)
         @info "Stage2, max_try = $max_try, next_try = $next_try, checking if vaild"
         flag = check_vaild_time(sp, next_try, Vtrans)
         if flag == false
@@ -257,10 +266,11 @@ end
 # evaluate_adiabatic_time(3, 3)
 # check_vaild_time(spinglass_mapping(3, 3), 1, fill(1.0, 3*3+3*2))
 
-
-
-function vectorgradient(vec, this_t, n, m)
+function vectorgradient(vec, this_t, n, m; freeze=false)
     sp = spinglass_mapping(n, m)
+    if freeze == true
+        freeze_input!(sp)
+    end
     sp.M = [Vector(vec[i:i+2]) for i in 1:3:3*length(sp.onsite)]
     Vtrans = Vector(vec[end-length(sp.onsite)+1:end])
     Tmax = Float64(vec[3*length(sp.onsite) + 1])
@@ -292,14 +302,14 @@ function initialvector(Tmax, n, m)
 end
 
 function fcn(x, y, f)
-    g = vectorgradient(y, x, 3, 3)
+    g = vectorgradient(y, x, 3, 3;freeze=true)
     for i in 1:length(g)
         f[i] = g[i]
     end
 end
 
 function spingls!(du, u, p, t)
-    g = vectorgradient(u, t, 3, 3)
+    g = vectorgradient(u, t, 3, 3;freeze=true)
     for i in 1:length(g)
         du[i] = g[i]
     end
@@ -313,13 +323,22 @@ function gproject(resid, u, p, t)
     end
 end
 
+function printsp(sp::spinglassmodel)
+    for i in 1:length(sp.onsite)
+        for j in 1:3
+            print(sp.M[i][j]," ")
+        end
+        println("")
+    end
+end
+
 @testset "runge_kutta_vs_dmprince" begin
     Tmax = 100.0
     init_dp5 = initialvector(Tmax, 3, 3)
     init_dp8 = initialvector(Tmax, 3, 3)
-    solver5 = DP5Solver(fcn, 0.0, init_dp5; atol=1e-12, rtol=1e-12, maximum_allowed_steps=500000)
+    solver5 = DP5Solver(fcn, 0.0, init_dp5; atol=1e-14, rtol=1e-14, maximum_allowed_steps=5000000)
     integrate!(solver5, Tmax)
-    solver8 = DP8Solver(fcn, 0.0, init_dp8; atol=1e-12, rtol=1e-12, maximum_allowed_steps=500000)
+    solver8 = DP8Solver(fcn, 0.0, init_dp8; atol=1e-14, rtol=1e-14, maximum_allowed_steps=5000000)
     integrate!(solver8, Tmax)
 
     init_de = initialvector(Tmax, 3, 3)
@@ -331,24 +350,74 @@ end
     sp_this5 = vector2sp(get_current_state(solver5), 3, 3)
     sp_this8 = vector2sp(get_current_state(solver8), 3, 3)
     sp_de = vector2sp(sol[end], 3, 3)
-    for i in 1:length(sp_this5.onsite)
-        for j in 1:3
-            @test abs(sp_this5.M[i][j] - sp_de.M[i][j]) <= 1
-            # @test abs(sp_this8.M[i][j] - sp_de.M[i][j]) <= 1e-2
-            # @test abs(sp_this5.M[i][j] - sp_this8.M[i][j]) <= 1
-        end
-    end
+    # @test check_vaild(sp_this5)
+    # @test check_vaild(sp_this8)
+    # @test check_vaild(sp_de)
+    # for i in 3*length(sp_this5.onsite)+2:length(get_current_state(solver5))
+    #     @test abs(get_current_state(solver5)[i]-1) <= 1e-12
+    # end
+    @info "this is dp5"
+    printsp(sp_this5)
+    @info "this is dp8"
+    printsp(sp_this8)
+    @info "this is de"
+    printsp(sp_de)
+    # for i in 1:length(sp_this5.onsite)
+    #     for j in 1:3
+    #         @test abs(sp_this5.M[i][j] - sp_de.M[i][j]) <= 1e-1
+    #         @test abs(sp_this8.M[i][j] - sp_de.M[i][j]) <= 1e-1
+    #         @test abs(sp_this5.M[i][j] - sp_this8.M[i][j]) <= 1e-1
+    #     end
+    # end
     sp_r = spinglass_mapping(3, 3)
     Vtrans = fill(1.0, length(sp_r.onsite))
-    # runge_kutta_integrate!(sp_r, 1e-4, Tmax, Vtrans)
-    for i in 1:length(sp_r.onsite)
-        @test abs(1 - sum(sp_de.M[i] .* sp_de.M[i])) <= 1e-5
-        @test abs(1 - sum(sp_this5.M[i] .* sp_this5.M[i])) <= 1e-5
-        @test abs(1 - sum(sp_this8.M[i] .* sp_this8.M[i])) <= 1e-5
-        @info "i = $i, sp_de.M = $(sp_de.M[i])"
-        @info "i = $i, sp_this5.M = $(sp_this5.M[i])"
+    runge_kutta_integrate!(sp_r, 1e-2, Tmax, Vtrans)
+    @info "this is sp_r"
+    printsp(sp_r)
+    # @info "now calculate with runge_kutta"
+    # for i in 1:length(sp_r.onsite)
+    #     # @test abs(1 - sum(sp_de.M[i] .* sp_de.M[i])) <= 1e-5
+    #     # @test abs(1 - sum(sp_this5.M[i] .* sp_this5.M[i])) <= 1e-5
+    #     # @test abs(1 - sum(sp_this8.M[i] .* sp_this8.M[i])) <= 1e-5
+    #     # @info "i = $i, sp_de.M = $(sp_de.M[i])"
+    #     # @info "i = $i, sp_this5.M = $(sp_this5.M[i])"
+    #     for j in 1:3
+    #         print(sp_r.M[i][j]," ")
+    #         # @test abs(sp_r.M[i][j] - sp_de.M[i][j]) <= 1e-3
+    #     end
+    #     println("")
+    # end
+end
+
+
+@testset "runge_kutta_error" begin
+    sp_r1 = spinglass_mapping(3, 3)
+    sp_r2 = spinglass_mapping(3, 3)
+    Vtrans = fill(1.0, length(sp_r1.onsite))
+    runge_kutta_integrate!(sp_r1, 1e-4, 100.0, Vtrans; T_end = 100.0)
+    runge_kutta_integrate!(sp_r2, 1e-3, 100.0, Vtrans; T_end = 100.0)
+    # for i in 1:length(sp_r1.onsite)
+    #     @info "sp_r1.M = $(sp_r1.M[i]), sp_r2.M = $(sp_r2.M[i])"
+    #     @info "delta = $(sp_r1.M[i] .- sp_r2.M[i])"
+    # end
+
+    for i in 1:length(sp_r1.onsite)
         for j in 1:3
-            # @test abs(sp_r.M[i][j] - sp_de.M[i][j]) <= 1e-3
+            @test abs(sp_r1.M[i][j] - sp_r2.M[i][j]) <= 1e-5
+        end
+    end
+end
+
+@testset "runge_kutta_with_euclidean" begin
+    sp_r = spinglass_mapping(3, 2)
+    sp_e = spinglass_mapping(3, 2)
+    Vtrans = fill(1.0, length(sp_r.onsite))
+    runge_kutta_integrate!(sp_r, 1e-2, 10.0, Vtrans)
+    euclidean_integrate!(sp_e, 1e-6, 10.0, Vtrans)
+
+    for i in 1:length(sp_r.onsite)
+        for j in 1:3
+            @test abs(sp_r.M[i][j] - sp_e.M[i][j]) <= 1e-2
         end
     end
 end
