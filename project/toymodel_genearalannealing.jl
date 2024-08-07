e8e6d3140858924e9055223ff13e401048f2697d
Rose-max111 <luyimingboy@163.com>
2024-08-07 12:43:52 +0000

    fixed some issues

--- project/toymodel_genearalannealing.jl
+++ project/toymodel_genearalannealing.jl
@@ -1,21 +1,21 @@
 using QuantumAdiabaticAnnealing
 using QuantumAdiabaticAnnealing: random_state, calculate_energy
-using QuantumAdiabaticAnnealing: track_equilibration_gausspulse_reverse_cpu!
+using QuantumAdiabaticAnnealing: track_equilibration_gausspulse_gpu!
+using CUDA
 
 function evaluate_50percent_time_cpu(width::Integer, depth::Integer, gauss_width, energy_gradient)
     sa = SimulatedAnnealingHamiltonian(width, depth)
-    nbatch = 300
-    energy_gradient_sa = fill(Float64(energy_gradient), nbatch)
+    nbatch = 1000
 
     anneal_time = 0
-    max_try = 1
+    max_try = 2
     while max_try > 0
         next_try = anneal_time + max_try
         
         state = random_state(sa, nbatch)
         @info "Stage1, max_try = $max_try, next_try = $next_try, begin annealing"
         # @info "$Temp_sa"
-        @time track_equilibration_gausspulse_cpu!(HeatBath(), sa, state, energy_gradient_sa, 10.0, gauss_width, next_try)
+        @time track_equilibration_gausspulse_cpu!(HeatBath(), sa, state, energy_gradient, 10.0, gauss_width, next_try)
         @info "finish annealing"
 
         state_energy = [calculate_energy(sa, state, fill(1.0, nbatch), i) for i in 1:nbatch]
@@ -38,18 +38,19 @@ function evaluate_50percent_time_cpu(width::Integer, depth::Integer, gauss_width
         state = random_state(sa, nbatch)
         @info "Stage2, max_try = $max_try, next_try = $next_try, begin annealing"
         # @info "$Temp_sa"
-        @time track_equilibration_gausspulse_cpu!(HeatBath(), sa, state, energy_gradient_sa, 10.0, gauss_width, next_try)
+        @time track_equilibration_gausspulse_cpu!(HeatBath(), sa, state, energy_gradient, 10.0, gauss_width, next_try)
         @info "finish annealing"
 
         state_energy = [calculate_energy(sa, state, fill(1.0, nbatch), i) for i in 1:nbatch]
         success = count(x -> x == 0, state_energy)
         # @info "Stage 2, now max_try = $max_try, success time = $success, anneal_time = $anneal_time"
         if 1.0 * success / nbatch >= 0.49
+            max_try /=2
             continue
         else
+            max_try /=2
             anneal_time = next_try
         end
-        max_try /=2
     end
     return anneal_time
 end
@@ -57,18 +58,17 @@ end
 
 function evaluate_50percent_time_gpu(width::Integer, depth::Integer, gauss_width, energy_gradient)
     sa = SimulatedAnnealingHamiltonian(width, depth)
-    nbatch = 300
-    energy_gradient_sa = CuArray(fill(Float32(energy_gradient), nbatch))
+    nbatch = 1000
 
-    anneal_time = 0
-    max_try = 1
+    anneal_time = 0.0
+    max_try = 2.0
     while max_try > 0
         next_try = anneal_time + max_try
         
         state = CuArray(random_state(sa, nbatch))
         @info "Stage1, max_try = $max_try, next_try = $next_try, begin annealing"
         # @info "$Temp_sa"
-        @time track_equilibration_gausspulse_gpu!(HeatBath(), sa, state, energy_gradient_sa, 10.0, gauss_width, next_try)
+        @time track_equilibration_gausspulse_gpu!(HeatBath(), sa, state, energy_gradient, 10.0, gauss_width, next_try)
         @info "finish annealing"
 
         cpu_state = Array(state)
@@ -92,7 +92,7 @@ function evaluate_50percent_time_gpu(width::Integer, depth::Integer, gauss_width
         state = CuArray(random_state(sa, nbatch))
         @info "Stage2, max_try = $max_try, next_try = $next_try, begin annealing"
         # @info "$Temp_sa"
-        @time track_equilibration_gausspulse_gpu!(HeatBath(), sa, state, energy_gradient_sa, 10.0, gauss_width, next_try)
+        @time track_equilibration_gausspulse_gpu!(HeatBath(), sa, state, energy_gradient, 10.0, gauss_width, next_try)
         @info "finish annealing"
 
         cpu_state = Array(state)
@@ -100,25 +100,29 @@ function evaluate_50percent_time_gpu(width::Integer, depth::Integer, gauss_width
         success = count(x -> x == 0, state_energy)
         # @info "Stage 2, now max_try = $max_try, success time = $success, anneal_time = $anneal_time"
         if 1.0 * success / nbatch >= 0.49
+            max_try /=2
             continue
         else
+            max_try /=2
             anneal_time = next_try
         end
-        max_try /=2
     end
     return anneal_time
 end
 
 
-# sa = SimulatedAnnealingHamiltonian(6, 12)
-# nbatch = 100
-# energy_gradient_sa = fill(1.2, nbatch)
-# state = random_state(sa, nbatch)
+sa = SimulatedAnnealingHamiltonian(8, 8)
+nbatch = 1000
+# energy_gradient_sa = fill(1.5, nbatch)
+state = CuArray(random_state(sa, nbatch))
 # state_energy = [calculate_energy(sa, state, energy_gradient_sa, i) for i in 1:nbatch]
 
-# # track_equilibration_gausspulse_reverse_cpu!(HeatBath(), sa, state, energy_gradient_sa, 10.0, 2.0, 500)
-
-# track_equilibration_gausspulse_cpu!(HeatBath(), sa, state, energy_gradient_sa, 10.0, 20.0, 10000)
-
+# track_equilibration_gausspulse_cpu!(HeatBath(), sa, state, energy_gradient_sa, 10.0, 10.0, 512)
 # state_energy = [calculate_energy(sa, state, fill(1.0, nbatch), i) for i in 1:nbatch]
 # success = count(x -> x == 0, state_energy)
+
+track_equilibration_gausspulse_gpu!(HeatBath(), sa, state, 1.5, 10.0, 10.0, 1800)
+
+cpu_state = Array(state)
+state_energy = [calculate_energy(sa, cpu_state, fill(1.0, nbatch), i) for i in 1:nbatch]
+success = count(x -> x == 0, state_energy)
