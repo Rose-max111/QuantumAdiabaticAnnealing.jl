7d8e6d0d4750d7c81f5eceb64d0226df0964d4b1
Yiming Lu <luyimingboy@163.com>
2024-08-14 17:07:30 +0800

    update notes

--- project/myplot.jl
+++ project/myplot.jl
@@ -15,8 +15,8 @@ end
 
 function draw_timevsdepth(xdata, ydata)
     f = Figure()
-    ax = Axis(f[1, 1], xlabel = "depth * log(depth)", ylabel = "sweep times",
-        title = "Time v.s. Depth(50% success probability, λ=1.5, width=12)")
+    ax = Axis(f[1, 1], xlabel = "depth * log^2(depth)", ylabel = "sweep times",
+        title = "Time v.s. Depth(50% success probability, Λ=1.3, width=15) -- Energy Gradient")
     scatter!(ax, xdata, ydata)
 
     # fit = curve_fit(Polynomial, xdata, ydata, 2)
@@ -31,7 +31,8 @@ end
 function __main__(graph_width, graph_depth, λ, GW)
     evaluate_time = Vector{Float64}()
     for d in graph_depth
-        filepath = joinpath(@__DIR__, "data_toymodel_pulse/W=$(graph_width)_D=$(d)_GW=$(GW)_E=$(λ).txt")
+        # filepath = joinpath(@__DIR__, "data_toymodel_pulse/W=$(graph_width)_D=$(d)_GW=$(GW)_E=$(λ).txt")
+        filepath = joinpath(@__DIR__, "data_toymodel/W=$(graph_width)_D=$(d)_E=$(λ).txt")
         open(filepath, "r") do file
             push!(evaluate_time, parse(Float64, readline(file)))
         end
@@ -50,4 +51,5 @@ function __main__(graph_width, graph_depth, λ, GW)
     # end
 end
 
-__main__(12, [6, 8, 10, 12, 15, 18, 22, 25, 28, 31, 34, 36, 39, 42, 45, 48, 50, 53, 56, 58, 61, 64, 67, 69, 72, 74, 77, 80, 82, 85, 88, 90, 92], 1.5, 1.0)
+# __main__(15, [4, 6, 8, 10, 12, 15, 18, 20, 22, 25, 28, 30, 33, 35, 37, 40, 42, 44, 46, 49, 52, 54], 1.3, 1.0)
+__main__(15, [4, 6, 8, 10, 12, 15, 18, 20, 22, 25, 28, 30, 33, 35, 37], 1.3, 1.0)
