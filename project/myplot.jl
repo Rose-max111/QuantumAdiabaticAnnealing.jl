fdf3e0ffa6b625ba38b697c42b91eba77b61076d
Yiming Lu <luyimingboy@163.com>
2024-09-12 15:14:26 +0800

    data#

--- project/myplot.jl
+++ project/myplot.jl
@@ -28,7 +28,7 @@ end
 
 # draw(xx[1:10], yy[1:10])
 
-function __main__(graph_width, graph_depth, λ, GW)
+function __main__(coef, graph_width, graph_depth, λ, GW)
     evaluate_time = Vector{Float64}()
     for d in graph_depth
         filepath = joinpath(@__DIR__, "data_toymodel_pulse/W=$(graph_width)_D=$(d)_GW=$(GW)_E=$(λ).txt")
@@ -38,7 +38,16 @@ function __main__(graph_width, graph_depth, λ, GW)
         end
     end
     graph_depth = Float64.(graph_depth)
-    draw_timevsdepth(((graph_depth) .* log.(graph_depth) .* log.(graph_depth))[1:end], (evaluate_time[1:end]))
+    ydata = evaluate_time ./ graph_depth
+    xdata = log.(graph_depth)
+    for i in xdata
+        println(i)
+    end
+    for i in ydata
+        println(i)
+    end
+    xdata = [x * log(x) ^ coef for x in graph_depth]
+    draw_timevsdepth(xdata, (evaluate_time[1:end]))
 
     # filepath = joinpath(@__DIR__, "test.txt")
     # open(filepath, "w") do file
@@ -74,8 +83,10 @@ function __main__gradient(graph_width, graph_depth, λ)
     # end
 end
 
+val = 3
+__main__(1 + 1 / (log(1.5) * val), 12, [12, 15, 18, 22, 25, 28, 31, 34, 36, 39, 42, 45, 48, 50, 53, 56, 58, 61, 64, 67, 69, 72, 74, 77, 80, 82, 85, 88, 90, 92], 1.5, 1.0)
+__main__(1 + 1 / (log(1.3) * val), 15, [8, 10, 12, 15, 18, 20, 22, 25, 28, 30, 33, 35, 37], 1.3, 1.0)
+__main__(1 + 1 / (log(1.24) * val), 14, [10, 13, 16, 18, 21, 24, 27, 30, 32, 35, 38, 40], 1.24, 1.0)
+__main__(1 + 1 / (log(1.18) * val), 13, [10, 12, 15, 18, 20, 22, 25, 28, 30], 1.18, 1.0)
 
-# __main__(12, [6, 8, 10, 12, 15, 18, 22, 25, 28, 31, 34, 36, 39, 42, 45, 48, 50, 53, 56, 58, 61, 64, 67, 69, 72, 74, 77, 80, 82, 85, 88, 90, 92], 1.5, 1.0)
-# __main__(15, [4, 6, 8, 10, 12, 15, 18, 20, 22, 25, 28, 30, 33, 35, 37], 1.3, 1.0)
-
-__main__gradient(15, [4, 6, 8, 10, 12, 15, 18, 20, 22, 25, 28, 30, 33, 35, 37], 1.3)
+# __main__gradient(15, [4, 6, 8, 10, 12, 15, 18, 20, 22, 25, 28, 30, 33, 35, 37], 1.3)
--- project/paperplot.py
+++ project/paperplot.py
@@ -6,13 +6,13 @@ import re
 import matplotlib.ticker as mticker
 
 
-def read_depth_file(addpath):
+def read_depth_file(addpath, width):
     folder_path = os.getcwd() + addpath
     xvalue = []
     yvalue = []
     for filename in os.listdir(folder_path):
         # print(filename)
-        if filename.startswith("W=15_D="):
+        if filename.startswith(f"W={width}_D="):
             file_path = os.path.join(folder_path, filename)
             d_value = float(filename.split('_')[1][2:])
             with open(file_path, 'r') as file:
@@ -26,7 +26,7 @@ def read_depth_file(addpath):
     xvalue, yvalue = zip(*sorted_pairs)
     xvalue = np.array(xvalue).astype(float)
     yvalue = np.array(yvalue).astype(float)
-    xvalue *= np.log(xvalue) * np.log(xvalue)
+    # xvalue *= np.log(xvalue) * np.log(xvalue)
     return xvalue, yvalue
 
 
@@ -60,17 +60,17 @@ def read_width_file(addpath, number):
 
 def draw1():
 
-    xvalue_gradient, yvalue_gradient = read_depth_file('/data_toymodel/')
-    xvalue_pulse, yvalue_pulse = read_depth_file('/data_toymodel_pulse/')
+    xvalue_gradient, yvalue_gradient = read_depth_file('/data_toymodel/', 15)
+    xvalue_pulse, yvalue_pulse = read_depth_file('/data_toymodel_pulse/', 15)
 
-    coefficients = np.polyfit(
-        xvalue_gradient, yvalue_gradient, 1)  # 1 表示一次多项式（线性）
-    slope1, intercept1 = coefficients
-    y_fit_gradient = slope1 * xvalue_pulse + intercept1
+    # coefficients = np.polyfit(
+    #     xvalue_gradient, yvalue_gradient, 1)  # 1 表示一次多项式（线性）
+    # slope1, intercept1 = coefficients
+    # y_fit_gradient = slope1 * xvalue_pulse + intercept1
 
-    coefficients = np.polyfit(xvalue_pulse, yvalue_pulse, 1)  # 1 表示一次多项式（线性）
-    slope2, intercept2 = coefficients
-    y_fit_pulse = slope2 * xvalue_pulse + intercept2
+    # coefficients = np.polyfit(xvalue_pulse, yvalue_pulse, 1)  # 1 表示一次多项式（线性）
+    # slope2, intercept2 = coefficients
+    # y_fit_pulse = slope2 * xvalue_pulse + intercept2
 
     # fig = plt.figure(dpi=200, figsize=(20, 5.5))
     fig = plt.figure(dpi=200)
@@ -80,12 +80,12 @@ def draw1():
     ax1 = plt.subplot(1, 1, 1)
     ax1.scatter(xvalue_gradient, yvalue_gradient, marker='o', edgecolors='#9a031e',
                 facecolors='none', linewidth=3, s=60, label='Energy Gradient', zorder=5)
-    ax1.plot(xvalue_pulse, y_fit_gradient, '--', c='#03045e',
-             linewidth=2.5, zorder=5, label='Fit of Energy Gradient')
+    # ax1.plot(xvalue_pulse, y_fit_gradient, '--', c='#03045e',
+    #          linewidth=2.5, zorder=5, label='Fit of Energy Gradient')
     ax1.scatter(xvalue_pulse, yvalue_pulse, marker='o', edgecolors='#fcbf49',
-                facecolors='none', linewidth=3, s=60, label='Wave Packet')
-    ax1.plot(xvalue_pulse, y_fit_pulse, '-', c='#0096c7',
-             linewidth=2.5, zorder=3, label='Fit of Wave Packet')
+                facecolors='none', linewidth=3, s=60, label='Moving Heat Bath')
+    # ax1.plot(xvalue_pulse, y_fit_pulse, '-', c='#0096c7',
+    #          linewidth=2.5, zorder=3, label='Fit of Wave Packet')
 
     ax = plt.gca()
     mylinewidth = 1.5
@@ -94,20 +94,20 @@ def draw1():
     ax.spines['top'].set_linewidth(mylinewidth)
     ax.spines['right'].set_linewidth(mylinewidth)
 
-    plt.xlim(left=0, right=550)
+    plt.xlim(left=0, right=34)
     plt.ylim(bottom=0, top=65000)
 
     yticks_name = ['0'] + [f'{y}'+r'$\times 10^4$' for y in [
         1, 2, 3, 4, 5, 6]]
     yticks = [y*1e4 for y in [0, 1, 2, 3, 4, 5, 6]]
     # print(yticks_name)
-    plt.xticks(fontsize=12)
-    plt.yticks(yticks, yticks_name, fontsize=12, fontname='Times New Roman')
+    plt.xticks(fontsize=15)
+    plt.yticks(yticks, yticks_name, fontsize=15, fontname='Times New Roman')
 
-    plt.xlabel(r'${D} \log^2(D)$', fontsize=15)
-    plt.ylabel('Sweep Times', fontsize=15)
+    plt.xlabel(r'${M}$', fontsize=20)
+    plt.ylabel(r'$T_c$', fontsize=20)
 
-    plt.legend(fontsize=14, loc='upper left', frameon=False)
+    plt.legend(fontsize=16, loc='upper left', frameon=False)
 
     plt.tight_layout()
 
@@ -135,13 +135,13 @@ def draw2():
 
     ax1 = plt.subplot(1, 1, 1)
     ax1.scatter(xvalue_20, yvalue_20, marker='o', edgecolors='#9a031e',
-                facecolors='none', linewidth=3, s=60, label='Depth=20', zorder=5)
+                facecolors='none', linewidth=3, s=60, label='T=20', zorder=5)
     ax1.plot(xvalue_20, y_fit_20, '--', c='#03045e',
-             linewidth=2.5, zorder=5, label='Fit of Depth=20')
+             linewidth=2.5, zorder=5, label='Fit of T=20')
     ax1.scatter(xvalue_30, yvalue_30, marker='o', edgecolors='#fcbf49',
-                facecolors='none', linewidth=3, s=60, label='Depth=30')
+                facecolors='none', linewidth=3, s=60, label='T=30')
     ax1.plot(xvalue_30, y_fit_30, '-', c='#0096c7',
-             linewidth=2.5, zorder=3, label='Fit of Depth=30')
+             linewidth=2.5, zorder=3, label='Fit of T=30')
     ax = plt.gca()
     mylinewidth = 1.5
     ax.spines['bottom'].set_linewidth(mylinewidth)
@@ -159,7 +159,7 @@ def draw2():
     plt.xticks(fontsize=12)
     plt.yticks(yticks, yticks_name, fontsize=12, fontname='Times New Roman')
 
-    plt.xlabel('Width', fontsize=15)
+    plt.xlabel('T', fontsize=15)
     plt.ylabel('Sweep Times', fontsize=15)
 
     plt.legend(fontsize=14, loc='lower right', frameon=False)
@@ -169,7 +169,130 @@ def draw2():
     plt.show()
 
 
-draw2()
+def modify(xdata, ydata):
+    ydata = np.log(ydata / xdata)
+    xdata = np.log(np.log(xdata))
+    return xdata, ydata
+
+
+def draw3():
+    fontsize_axis = 15
+    fontsize_label = 20
+    xdata1_5, ydata1_5 = read_depth_file('/data_toymodel_pulse/', 12)
+    xdata1_5, ydata1_5 = modify(xdata1_5, ydata1_5)
+    xdata1_3, ydata1_3 = read_depth_file('/data_toymodel_pulse/', 15)
+    xdata1_3, ydata1_3 = modify(xdata1_3, ydata1_3)
+    xdata1_24, ydata1_24 = read_depth_file('/data_toymodel_pulse/', 14)
+    xdata1_24, ydata1_24 = modify(xdata1_24, ydata1_24)
+    xdata1_18, ydata1_18 = read_depth_file('/data_toymodel_pulse/', 13)
+    xdata1_18, ydata1_18 = modify(xdata1_18, ydata1_18)
+
+    fig = plt.figure(dpi=200, figsize=(30, 5.5))
+    plt.rcParams['mathtext.fontset'] = 'stix'
+    plt.rcParams['font.family'] = 'Times New Roman'
+
+    ax1 = plt.subplot(1, 2, 1)
+    ax1.scatter(xdata1_5, ydata1_5, marker='o', edgecolors='#273e47',
+                facecolors='none', linewidth=3, s=60, label=r'$\Lambda = 1.5$', zorder=5)
+    coefficients = np.polyfit(
+        xdata1_5, ydata1_5, 1)  # 1 表示一次多项式（线性）
+    slope1, intercept1 = coefficients
+    ax1.scatter(xdata1_3, ydata1_3, marker='s', edgecolors='#bd632f',
+                facecolors='none', linewidth=3, s=60, label=r'$\Lambda = 1.3$', zorder=5)
+    coefficients = np.polyfit(
+        xdata1_3, ydata1_3, 1)  # 1 表示一次多项式（线性）
+    slope2, intercept2 = coefficients
+    ax1.scatter(xdata1_24, ydata1_24, marker='^', edgecolors='#d8973c',
+                facecolors='none', linewidth=3, s=60, label=r'$\Lambda = 1.24$', zorder=5)
+    coefficients = np.polyfit(
+        xdata1_24, ydata1_24, 1)  # 1 表示一次多项式（线性）
+    slope3, intercept3 = coefficients
+    ax1.scatter(xdata1_18, ydata1_18, marker='D', edgecolors='#d8c99b',
+                facecolors='none', linewidth=3, s=60, label=r'$\Lambda = 1.18$', zorder=5)
+    coefficients = np.polyfit(
+        xdata1_18, ydata1_18, 1)  # 1 表示一次多项式（线性）
+    slope4, intercept4 = coefficients
+
+    # print(slope1, slope2, slope3, slope4)
+
+    xvalue_20, yvalue_20 = read_width_file(
+        '/data_toymodel_pulse/rule110/width_sweep', 20)
+    xvalue_30, yvalue_30 = read_width_file(
+        '/data_toymodel_pulse/rule110/width_sweep', 30)
+    coefficients = np.polyfit(
+        xvalue_20, yvalue_20, 1)  # 1 表示一次多项式（线性）
+    slope1, intercept1 = coefficients
+    y_fit_20 = slope1 * xvalue_20 + intercept1
+    coefficients = np.polyfit(
+        xvalue_30, yvalue_30, 1)  # 1 表示一次多项式（线性）
+    slope2, intercept2 = coefficients
+    y_fit_30 = slope2 * xvalue_30 + intercept2
+
+    ax2 = plt.subplot(1, 2, 2)
+    ax2.scatter(xvalue_20, yvalue_20, marker='o', edgecolors='#9a031e',
+                facecolors='none', linewidth=3, s=60, label='M=20', zorder=5)
+    ax2.plot(xvalue_20, y_fit_20, '--', c='#03045e',
+             linewidth=2.5, zorder=5, label='Fit of M=20')
+    ax2.scatter(xvalue_30, yvalue_30, marker='o', edgecolors='#fcbf49',
+                facecolors='none', linewidth=3, s=60, label='M=30')
+    ax2.plot(xvalue_30, y_fit_30, '-', c='#0096c7',
+             linewidth=2.5, zorder=3, label='Fit of M=30')
+
+    ax = plt.gca()
+    mylinewidth = 1.5
+    ax1.spines['bottom'].set_linewidth(mylinewidth)
+    ax1.spines['left'].set_linewidth(mylinewidth)
+    ax1.spines['top'].set_linewidth(mylinewidth)
+    ax1.spines['right'].set_linewidth(mylinewidth)
+
+    ax2.spines['bottom'].set_linewidth(mylinewidth)
+    ax2.spines['left'].set_linewidth(mylinewidth)
+    ax2.spines['top'].set_linewidth(mylinewidth)
+    ax2.spines['right'].set_linewidth(mylinewidth)
+
+    ax1.set_xlim(left=0.6, right=1.55)
+    ax1.set_ylim(bottom=4, top=10)
+    ax2.set_xlim(left=2, right=52)
+    ax2.set_ylim(bottom=2000, top=10500)
+
+    ax1.text(0.92, 0.98, '(a)', transform=ax1.transAxes,
+             fontsize=18, verticalalignment='top')
+    ax2.text(0.92, 0.98, '(b)', transform=ax2.transAxes,
+             fontsize=18, verticalalignment='top')
+
+    yticks_name = ['0'] + [f'{y}'+r'$\times 10^3$' for y in [
+        2, 4, 6, 8]] + [f'{y}' + r'$\times 10^4$' for y in [1]]
+    yticks = [y*1e3 for y in [0, 2, 4, 6, 8, 10]]
+    # print(yticks_name)
+    # ax2.set_xticks(fontsize=12)
+    ax2.set_yticks(yticks, yticks_name, fontsize=fontsize_axis,
+                   fontname='Times New Roman')
+    ax2.tick_params(axis='x', labelsize=fontsize_axis)
+    ax1.tick_params(axis='both', labelsize=fontsize_axis)
+
+    # plt.ylim(bottom=0)
+    # plt.xlim(left=0.6, right=1.55)
+    # plt.ylim(bottom=4, top=10)
+    ax1.set_xlabel(r'$\ln{(\ln(M))}$', fontsize=fontsize_label)
+    ax1.set_ylabel(
+        r'$\ln\left(v\right)$', fontsize=fontsize_label)
+
+    ax2.set_xlabel(r'$N$', fontsize=fontsize_label)
+    ax2.set_ylabel(r'$T_c$', fontsize=fontsize_label)
+    ax1.legend(fontsize=16, loc='upper left', frameon=False)
+    ax2.legend(fontsize=16, loc='upper left', frameon=False)
+
+    # plt.tight_layout()
+    plt.subplots_adjust(left=0.08, right=0.98, top=0.98,
+                        bottom=0.11, wspace=0.23, hspace=0.2)
+
+    plt.savefig('main_result.pdf', format='pdf')
+    plt.show()
+
+
+# draw3()
+draw1()
+# draw2()
 
 # yticks = [0, 0.5, 1, 1.5, 2, 3]
 # name_yticks = ['0', '1/2', '1', '3/2', '2', '2.5']
